6695611bb10d23a699d1fcf625a24fd388da1add
JPerfectSolution <j.perfectsolution@outlook.com>
2020-02-26 00:22:34 -0800

    update menu for mobile responsive

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -6,6 +6,9 @@ import { Dropdown } from "react-bootstrap";
 import DropdownToggle from "react-bootstrap/DropdownToggle";
 import DropdownMenu from "react-bootstrap/DropdownMenu";
 import DropdownItem from "react-bootstrap/DropdownItem";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowDown, faCheckCircle, faBars} from "@fortawesome/free-solid-svg-icons";
+import { DropdownDivider } from "react-bootstrap/Dropdown";
 const Header = ({history}) => {
   const dark=(history.location.pathname === "/case-study")?true:false;
   const [top, setTop]=useState(true);
@@ -29,6 +32,76 @@ const Header = ({history}) => {
           <img src={Logo} alt="FEVO LOGO" />
         </NavLink>
       </div>
+      <Dropdown className="menu-toggle-btn" alignRight>
+        <DropdownToggle as="div">
+          <FontAwesomeIcon icon={faBars} size="2x"/>
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem as="div">
+            <NavLink to="/" activeClassName="active-link" exact>
+              Home
+            </NavLink>
+          </DropdownItem>
+          <Dropdown.Divider />
+          <DropdownItem as="div">
+            <NavLink to="/brands/about-fevo" activeClassName="active-link" exact>
+              About Fevo
+            </NavLink>
+          </DropdownItem>
+          <DropdownItem as="div">
+            <NavLink to="/case-study" activeClassName="active-link" exact>
+              Case Studies
+            </NavLink>  
+          </DropdownItem>
+          <DropdownItem as="div">
+            <NavLink to="/brands/video-testimonials" activeClassName="active-link" exact>
+              Testimonials
+            </NavLink>
+          </DropdownItem>
+          <DropdownItem as="div">
+            <NavLink to="/brands" activeClassName="active-link" exact>
+              Blog
+            </NavLink>  
+          </DropdownItem>
+          <DropdownItem as="div">
+            <NavLink to="/brands" activeClassName="active-link" exact>
+              Work With Fevo
+            </NavLink>  
+          </DropdownItem>
+          <Dropdown.Divider />
+          <DropdownItem as="div">
+            <NavLink to="/fans/about-fevo" activeClassName="active-link" exact >
+              About Fevo - Fans
+            </NavLink>
+          </DropdownItem>
+          <DropdownItem as="div">
+            <NavLink to="/fans" activeClassName="active-link" exact>
+              Blog
+            </NavLink>
+          </DropdownItem>
+          <DropdownItem as="div">
+            <NavLink to="#" activeClassName="active-link" exact >
+              Order Support
+            </NavLink>
+          </DropdownItem>
+          <Dropdown.Divider />
+          <DropdownItem as="div">
+            <NavLink to="/case-study" activeClassName="active-link" exact>
+              Case Studies
+            </NavLink>
+          </DropdownItem>
+          <DropdownItem as="div">
+            <NavLink to="/press" activeClassName="active-link" exact>
+              Press
+            </NavLink>
+          </DropdownItem>
+          <DropdownItem as="div">
+            <NavLink to="/order-support" activeClassName="active-link" exact>
+              Order Support
+            </NavLink>
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
       <div className={(!dark || !top)?"menu":"menu dark"} >
         <NavLink to="/" activeClassName="active-link" exact>
           Home
