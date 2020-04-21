e0f843154c0d66caaa16dc09b5b78a8c06f74fbd
JPerfectSolution <j.perfectsolution@outlook.com>
2020-04-21 10:23:16 -0700

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 
 import "./App.scss";
 import Loading from "./components/Loading";
+import ScrollToTop from "./containers/ScrollToTop";
 const Layout = React.lazy(() => import("./containers/Layout"));
 const App = () => {
   return (
     <HashRouter>
+      <ScrollToTop />
       <React.Suspense fallback={<Loading />}>
         <Switch>
           <Route
---
+++ src/containers/ScrollToTop.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class ScrollToTop extends Component {
+    componentDidUpdate(prevProps) {
+        if (this.props.location !== prevProps.location) {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          })
+        }
+    }
+
+    render() {
+        return null
+    }
+}
+
+export default withRouter(ScrollToTop)
